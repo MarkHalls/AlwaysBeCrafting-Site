a34ddafceb2744f96de0409df53738ff5947aee5
Mark Halls <fireraisr@gmail.com>
2017-03-27 10:05:54 -0700

    removed unecessary navbar.js

--- js/navbar.js
+++
@@ -1,5 +0,0 @@
-const changeClass = ( id, className ) => {
-
-    document.querySelectorAll('.nav-link').forEach( x => x.classList.remove(className));
-    document.getElementById(id).classList.add(className);
-}
