6f32a0babc8a8458acb58a4e36eb9b28cddd80e6
Mark Halls <fireraisr@gmail.com>
2017-03-31 01:21:01 -0700

    moved chat commands into commands.js

--- js/chat.js
+++ js/chat.js
@@ -1,3 +1,4 @@
+
 let chatLog = [];
 let voteLog = [];
 
@@ -41,56 +42,14 @@ client.on("connecting", console.log);
 client.on("disconnected", console.error);
 
 client.on('message', (channel, user, message, self) => {
-    if (message[0] !== "!" && user.username !== token.username) {
+    if (user.username === token.username) {return;}
+    if (message[0] !== "!") {
         chatLog = [...chatLog, {timestamp: user["tmi-sent-ts"], user: user.username, message}];
-    }
-    const parsedMessage = message.trim().split(/\s+/);
-    switch (parsedMessage[0]) {
-        case '!veto':
-            console.log("Veto SUCCESS!!");
-            voteLog = [...voteLog, {trackid, downvote}];
-            next();
-            break;
-        case '!request':
-            console.log("Request SUCCESS!!");
-            voteLog = [...voteLog, {trackid, upvote}];
-            break;
-        case '!xkcd':
-            console.log("xkcd");
-            parsedMessage.shift();
-            const args = parsedMessage.join('%20');
-            console.log(args);
-            console.log(`http://localhost:3000/api/xkcd-proxy/${args}`);
-            axios.get(`http://localhost:3000/api/xkcd-proxy/${args}`)
-                .then(res => {
-                    console.log(res);
-                    client.say(channel, `${res.data.safe_title}, ${res.data.site}`)
-                        .catch(console.error);
-                })
-                .catch(console.error);
-            break;
-        case '!uptime':
-            console.log(channel.replace("#", ""));
-            axios.request({
-               url: `https://api.twitch.tv/kraken/streams/${channel.replace("#", "")}`, 
-               method: "get",
-               headers: {"Accept": "application/vnd.twitchtv.v3+json",
-                    "Client-ID": "vf9xv00vgz9ev65qvsk8suupgot5fr"}
-            })
-                .then(res => {
-                    if(res.data.stream) {
-                        console.log(res.data.stream);
-                        const uptime = Date.now() - new Date(res.data.stream.created_at);
-                        const hours = Math.floor(uptime / 1000 / 60 / 60 );
-                        const min = Math.floor(uptime / 1000 / 60 % 60);
-                        const sec = Math.floor(uptime / 1000 % 60);
-                        client.say(channel, `Uptime: ${hours}:${("0" + min).slice(-2)}:${("0" + sec).slice(-2)}`);
-                    }
-                })
-                .catch(console.error);
-            break;
-        default:
-            break;
+    } else {
+        const splitMessage = message.trim().split(/\s+/);
+        const commandName = splitMessage.shift().replace("!", "");
+        const command = commands[commandName];
+        command(splitMessage, channel);
     }
 });
 
